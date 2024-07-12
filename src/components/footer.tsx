02ae6ad63b37e9ee8c7009554e347248d7396ef9
Fed <punnaphop.sitthi@gmail.com>
2024-07-12 16:24:18 +0700

    💄 update header & footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import { siteConfig } from "@/config/site";
 
+import { Heart } from "lucide-react";
+
 export function Footer() {
     return (
-        <footer className="bg-background/95 border-t-2 border-border/40">
+        <footer className="bg-background/95 border-t">
             <div className="container h-footer max-w-screen-2xl flex items-center justify-between">
                 <span className="text-sm text-text/50">
                     &copy; 2024 {siteConfig.name}
                 </span>
                 <span className="text-sm text-text/50">
-                    Built with <span role="img" aria-label="heart">❤️</span> by{" "}
+                    Built with <Heart strokeWidth={0} fill="red" className="inline h-4 w-4" /> by {" "}
                     <Link href={siteConfig.links.github} className="underline">
                         {siteConfig.creator}
                     </Link>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ import { ThemeSwitcher } from "@/components/theme/switcher";
 
 export function Header() {
     return (
-        <header className="sticky top-0 z-50 w-full border-b-2 border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <header className="sticky top-0 z-50 w-full shadow-md dark:border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="container flex h-header max-w-screen-2xl items-center">
                 <div className="mr-6 flex items-center space-x-2">
                     <MobileNav />
