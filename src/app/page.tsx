70e8bc30f288646032b2d6683023f706979a07c2
Punnaphop Sitthiamornporn <punnaphop.sitthi@gmail.com>
2024-08-21 09:38:28 +0700

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
 import { validateRequest } from "@/lib/auth";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 
 export default async function Page() {
   const { user } = await validateRequest();
 
   return (
-    <div className="container p-8 flex justify-center">
-      <h1 className="text-4xl font-bold">{user ? `Welcome back, ${user.displayName}!` : "Welcome to OCOM Bruh!"}</h1>
+    <div className="flex min-h-screen-minus-header-footer flex-col items-center justify-center px-8 py-12">
+      <h1 className="mb-4 text-center text-5xl font-extrabold">
+        Read, Code, Submit, Pass!
+      </h1>
+      <p className="mb-8 max-w-xl text-center text-lg">
+        Practice your coding skills, prepare for contests, and become a god in
+        competitive programming. Start your journey today!
+      </p>
+      <div className="flex flex-col items-center gap-4 md:flex-row md:gap-8">
+        <Link href={user ? "/submission" : "/signup"}>
+          <Button className="text-lg font-semibold">
+            {user ? "Continue your journey" : "Get Started"}
+            <ArrowRight className="ml-2 h-5 w-5" />
+          </Button>
+        </Link>
+        <Link href="/problemset">
+          <Button variant="secondary" className="text-lg font-semibold">
+            Problems
+            <ArrowRight className="ml-2 h-5 w-5" />
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
