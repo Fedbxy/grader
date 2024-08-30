1054a297b0c52abbed632df9ef7606f99bd518a6
Punnaphop Sitthiamornporn <punnaphop.sitthi@gmail.com>
2024-08-31 03:23:40 +0700

    refactor: make it more simple

--- src/app/dashboard/problem/page.tsx
+++ src/app/dashboard/problem/page.tsx
@@ -2,14 +2,11 @@ import { allowAccess } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 import Link from "next/link";
 import { Plus } from "lucide-react";
-import { Problem } from "@/lib/types";
 
 import { columns } from "./columns";
 import { DataTable } from "@/components/table/data-table";
 import { Path } from "@/components/path";
 import { Button } from "@/components/ui/button";
-
-type databaseProblem = Omit<Problem, "statement">;
 
 export default async function Page() {
     await allowAccess("admin");
@@ -23,13 +20,6 @@ export default async function Page() {
         },
     });
 
-    const problems = data.map((problem: databaseProblem) => {
-        return {
-            ...problem,
-            statement: null,
-        };
-    });
-
     return (
         <div className="container flex flex-col space-y-2 mx-auto py-10">
             <h1 className="text-2xl font-semibold">Problems</h1>
@@ -39,7 +29,7 @@ export default async function Page() {
                     <Button size="icon"><Plus /></Button>
                 </Link>
             </div>
-            <DataTable columns={columns} data={problems} />
+            <DataTable columns={columns} data={data} />
         </div>
     );
 }
--- src/app/problemset/page.tsx
+++ src/app/problemset/page.tsx
@@ -1,11 +1,8 @@
 import { Metadata } from "next";
 import prisma from "@/lib/prisma";
-import { Problem } from "@/lib/types";
 
 import { columns } from "./columns";
 import { DataTable } from "@/components/table/data-table";
-
-type databaseProblem = Omit<Problem, "statement">;
 
 export const metadata: Metadata = {
     title: "Problems",
@@ -24,18 +21,11 @@ export default async function Page() {
         },
     });
 
-    const problems = data.map((problem: databaseProblem) => {
-        return {
-            ...problem,
-            statement: null,
-        };
-    });
-
     return (
         <div className="container flex flex-col space-y-2 mx-auto py-10">
             <h1 className="text-2xl font-semibold">Problems</h1>
             <h2 className="text-sm text-muted-foreground">Select a problem to view its statement and submit a solution.</h2>
-            <DataTable columns={columns} data={problems} />
+            <DataTable columns={columns} data={data} />
         </div>
     );
 }
