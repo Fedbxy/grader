6f09390066d781cd914680c004d910a104e50ddf
Punnaphop Sitthiamornporn <punnaphop.sitthi@gmail.com>
2024-09-02 10:51:38 +0700

    feat: add verdict setting between accordion and classic style

--- src/app/submission/[id]/page.tsx
+++ src/app/submission/[id]/page.tsx
@@ -1,28 +1,12 @@
 import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import Link from "next/link";
-import { maps } from "@/config/messages";
 
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableRow,
-} from "@/components/ui/table";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { Path } from "@/components/path";
 import { Progress } from "@/components/ui/progress";
+import { Verdict } from "./verdict";
 
 export default async function Page({ params }: { params: { id: string } }) {
   if (isNaN(Number(params.id))) {
@@ -41,9 +25,36 @@ export default async function Page({ params }: { params: { id: string } }) {
   }
 
   const data = {
-    Score: <div className="flex flex-col"><span className="text-xs">{submission.score * submission.problem.score / submission.problem.testcases} pts.</span><Progress className="h-2 w-32" value={submission.score * 100 / submission.problem.testcases} /></div>,
-    Problem: <a href={`/api/problem/${submission.problem.id}/statement`} target="_blank" className="hover:underline">{submission.problem.title}</a>,
-    User: <Link href={`/user/${submission.user.id}/profile`} className="hover:underline">{submission.user.displayName}</Link>,
+    Score: (
+      <div className="flex flex-col">
+        <span className="text-xs">
+          {(submission.score * submission.problem.score) /
+            submission.problem.testcases}{" "}
+          pts.
+        </span>
+        <Progress
+          className="h-2 w-32"
+          value={(submission.score * 100) / submission.problem.testcases}
+        />
+      </div>
+    ),
+    Problem: (
+      <a
+        href={`/api/problem/${submission.problem.id}/statement`}
+        target="_blank"
+        className="hover:underline"
+      >
+        {submission.problem.title}
+      </a>
+    ),
+    User: (
+      <Link
+        href={`/user/${submission.user.id}/profile`}
+        className="hover:underline"
+      >
+        {submission.user.displayName}
+      </Link>
+    ),
     Date: new Date(submission.createdAt).toLocaleString(),
     Language: submission.language,
   };
@@ -71,46 +82,8 @@ export default async function Page({ params }: { params: { id: string } }) {
                 ))}
               </TableBody>
             </Table>
-            {submission.verdict.length === 1 && !((submission.verdict[0] as keyof typeof maps.submission.verdict) in maps.submission.verdict) ? (
-              <div className="rounded-md p-4 bg-secondary">
-                {submission.verdict[0]}
-              </div>
-            ) : (
-              <Accordion type="multiple" className="space-y-2">
-                {submission.verdict.map((v, i) => (
-                  <AccordionItem
-                    key={i}
-                    value={`item-${i}`}
-                    className={`rounded-md px-4 text-xs ${v === "AC" ? "bg-constructive text-constructive-foreground" : "bg-destructive text-destructive-foreground"}`}
-                  >
-                    <AccordionTrigger>
-                      #{i + 1} {maps.submission.verdict[v as keyof typeof maps.submission.verdict]}
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      <Table className="text-xs">
-                        <TableBody>
-                          {submission.error && (
-                            <TableRow>
-                              <TableCell>Error</TableCell>
-                              <TableCell><pre>{submission.error}</pre></TableCell>
-                            </TableRow>
-                          )}
-                          <TableRow>
-                            <TableCell>Time</TableCell>
-                            <TableCell>{submission.time[i]}ms</TableCell>
-                          </TableRow>
-                          <TableRow>
-                            <TableCell>Memory</TableCell>
-                            <TableCell>{submission.memory[i]}KB</TableCell>
-                          </TableRow>
-                        </TableBody>
-                      </Table>
-                    </AccordionContent>
-                  </AccordionItem>
-                ))}
-              </Accordion>
-            )}
-            <div className="rounded-md p-4 overflow-x-auto bg-secondary">
+            <Verdict verdict={submission.verdict} error={submission.error} time={submission.time} memory={submission.memory} />
+            <div className="overflow-x-auto rounded-md bg-secondary p-4">
               <pre>{submission.code}</pre>
             </div>
           </div>
