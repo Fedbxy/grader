46fdde07eb1cfd01477535c5708e96196590ddd2
Punnaphop Sitthiamornporn <punnaphop.sitthi@gmail.com>
2024-09-18 23:18:07 +0700

    ops: add prisma initRootUser seed

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+import { hash } from "bcrypt";
+
+const prisma = new PrismaClient();
+
+async function initRootUser() {
+    const password = process.env.ROOT_PASSWORD || "00000000";
+    const passwordHash = await hash(password, 10);
+
+    await prisma.user.upsert({
+        where: {
+            id: 1,
+        },
+        update: {
+            password: passwordHash,
+            role: "admin",
+            isBanned: false,
+        },
+        create: {
+            id: 1,
+            username: "root",
+            displayName: "root",
+            password: passwordHash,
+            role: "admin",
+        },
+    });
+}
+
+initRootUser()
+    .then(async () => {
+        await prisma.$disconnect();
+    }).catch(async (error) => {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+    })
