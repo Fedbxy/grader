c6370e4f901abf1b2915081429fca71bbf0406c1
Punnaphop Sitthiamornporn <punnaphop.sitthi@gmail.com>
2024-10-24 14:21:38 +0700

    feat: add copied

--- src/app/submission/[id]/copy.tsx
+++ src/app/submission/[id]/copy.tsx
@@ -1,17 +1,29 @@
 "use client";
 
-import { Clipboard } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
+
+import { Check, Clipboard } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export function CopyButton({ code }: { code: string }) {
+  const [copied, setCopied] = useState(false);
+
+  function handleCopy() {
+    navigator.clipboard.writeText(code);
+    toast.success("Copied to clipboard");
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  }
+
   return (
     <Button
       className="h-8 w-8"
       variant="outline"
       size="icon"
-      onClick={() => navigator.clipboard.writeText(code)}
+      onClick={handleCopy}
     >
-      <Clipboard className="h-4 w-4" />
+      {copied ? <Check className="h-4 w-4" /> : <Clipboard className="h-4 w-4" />}
     </Button>
   );
 }
